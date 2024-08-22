74275ea3f3d53599100693d111d119868a009736
KanDai <KanDai@users.noreply.github.com>
2024-08-22 12:20:33 +0900

    fix

--- src/__tests__/calculateTax.test.ts
+++ src/__tests__/calculateTax.test.ts
@@ -1,27 +1,27 @@
 import { calculateTax } from '../sample02/calculateTax'
 
 describe('消費税計算', () => {
-    it('通常の消費税が適用されて100円が110円になる', () => {
-        expect(calculateTax(100)).toBe(110)
-    })
+  it('通常の消費税が適用されて100円が110円になる', () => {
+    expect(calculateTax(100)).toBe(110)
+  })
 
-    it('軽減税率が適用されて100円が108円になる', () => {
-        expect(calculateTax(100, true, false)).toBe(108)
-    })
+  it('軽減税率が適用されて100円が108円になる', () => {
+    expect(calculateTax(100, true, false)).toBe(108)
+  })
 
-    it('イートインのため通常の消費税で100円が110円になる', () => {
-        expect(calculateTax(100, false, true)).toBe(110)
-    })
+  it('イートインのため通常の消費税で100円が110円になる', () => {
+    expect(calculateTax(100, false, true)).toBe(110)
+  })
 
-    it('軽減税率対象だがイートインのため100円が110円になる', () => {
-        expect(calculateTax(100, true, true)).toBe(110)
-    })
+  it('軽減税率対象だがイートインのため100円が110円になる', () => {
+    expect(calculateTax(100, true, true)).toBe(110)
+  })
 
-    it('通常の消費税で切り捨てになる金額', () => {
-        expect(calculateTax(123, false, false)).toBe(135)
-    })
+  it('通常の消費税で切り捨てになる金額', () => {
+    expect(calculateTax(123, false, false)).toBe(135)
+  })
 
-    it('通常の消費税で切り上げになる金額', () => {
-        expect(calculateTax(198, false, false)).toBe(218)
-    })
+  it('通常の消費税で切り上げになる金額', () => {
+    expect(calculateTax(198, false, false)).toBe(218)
+  })
 })
--- src/__tests__/getDayOfWeek.test.ts
+++ src/__tests__/getDayOfWeek.test.ts
@@ -1,19 +1,19 @@
 import { getDayOfWeek } from '../sample03/getDayOfWeek'
 
 describe('曜日を取得', () => {
-    it('2023-4-13日は木曜日', () => {
-        expect(getDayOfWeek('2023-04-13')).toEqual({ en: 'Thu', ja: '木' })
-    })
+  it('2023-4-13日は木曜日', () => {
+    expect(getDayOfWeek('2023-04-13')).toEqual({ en: 'Thu', ja: '木' })
+  })
 
-    it('2023-12-31日は日曜日', () => {
-        expect(getDayOfWeek('2023-12-31')).toEqual({ en: 'Sun', ja: '日' })
-    })
+  it('2023-12-31日は日曜日', () => {
+    expect(getDayOfWeek('2023-12-31')).toEqual({ en: 'Sun', ja: '日' })
+  })
 
-    it('日付の書式が不正', () => {
-        expect(getDayOfWeek('2023年4月13日')).toBeUndefined()
-    })
+  it('日付の書式が不正', () => {
+    expect(getDayOfWeek('2023年4月13日')).toBeUndefined()
+  })
 
-    it('存在しない日付', () => {
-        expect(getDayOfWeek('2023-04-31')).toBeUndefined()
-    })
+  it('存在しない日付', () => {
+    expect(getDayOfWeek('2023-04-31')).toBeUndefined()
+  })
 })
